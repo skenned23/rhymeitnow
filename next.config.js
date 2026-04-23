b111d9238c4cd0f44caa6d7962f3d3253fbc8f2d
skenned23 <simplecashathome@gmail.com>
2026-04-23 12:53:55 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/yt',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
