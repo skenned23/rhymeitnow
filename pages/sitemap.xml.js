bca77e0cba07e94c565cc38f5f452933a8a6accc
skenned23 <simplecashathome@gmail.com>
2026-05-02 10:18:42 -0400

    Restore sitemap.xml.js

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,28 @@
+const WORDS = Object.keys(require('../data/words-content.json'))
+
+export default function Sitemap() {}
+
+export async function getServerSideProps({ res }) {
+  const baseUrl = 'https://rhymeitnow.com'
+const staticPages = ['', '/about', '/contact', '/rap-builder', '/freestyle', '/poetry-builder']
+  
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${staticPages.map(page => `  <url>
+    <loc>${baseUrl}${page}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`).join('\n')}
+${WORDS.map(word => `  <url>
+    <loc>${baseUrl}/rhymes-for/${word}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>`).join('\n')}
+</urlset>`
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(xml)
+  res.end()
+
+  return { props: {} }
+}
