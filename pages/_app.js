5a03c2d3a53f4b84a41a0b1f24d4d5f2ed7c1cb6
skenned23 <simplecashathome@gmail.com>
2026-05-08 11:45:10 -0400

    Add GA4 tracking to RhymeItNow

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-G9J2CD6H48"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G9J2CD6H48');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
